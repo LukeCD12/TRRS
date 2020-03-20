ab6b734654df1bcaa8a44987f269c2718bad4b49
DeGoes <degoesl@AEESINC.COM>
2020-03-20 12:26:57 -0500

    html update

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles'
 import firebase from '../firebase'
 import { withRouter, Link } from 'react-router-dom'
 import NavBar from '../NavBar'
+import './dashboard.css'
 
 const styles = theme => ({
     main: {
@@ -61,6 +62,7 @@ function Dashboard(props) {
     return (
         <div>
             <NavBar />
+            {/*<img src="https://doc-00-3g-docs.googleusercontent.com/docs/securesc/gkg7a9sflba9t6qb9q4gtc1ueb3jklc3/pmh584prart0q6h368sn93fhojnd4620/1584724650000/08125746697351790931/01384244190157029417/1a78qWQ5a-XNdSpU_U6HSI1_x--SV5LVn?e=view" alt="logo" />*/}
             <main className={classes.main}>
                 <Paper className={classes.paper}>
                     <Avatar alt="Profile Photo" src={photoSrc ? `${photoSrc}` : ''} className={classes.profile} />
